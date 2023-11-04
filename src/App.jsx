c6affb271f14a771a2ddeedc3f5fde684a34c2b5
antonio <antoniolm386@gmail.com>
2023-11-04 13:45:06 -0600

    proyect  structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,34 @@
 
 function App() {
     return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <main className="relative">
+        nav
+        <section className="xl:padding-1 wide:padding-r padding-b">
+          hero
+        </section>
+        <section className="padding">
+          popular products
+        </section>
+        <section className="padding">
+          super Quaity
+        </section>
+        <section className="padding-x py-10">
+          services
+        </section>
+        <section className="padding">
+          pspecial offer
+        </section>
+
+        <section className="padding bg-pale-blue">
+          customer reviews
+        </section>
+        <section className="padding-x sm:py-32 py-16 w-full">
+          subscribe
+        </section>
+        <section className="padding-x padding-t pb-8 bg-black">
+          footer
+        </section>
+      </main>
     )
   }
 export default App;
